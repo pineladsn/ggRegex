78f7e835f61196d52a8c4fe0d424ed008f9dd61a
pineladsn <bruno.pinelamarques@gmail.com>
2019-05-20 11:08:14 -0300

    Configure webpack and babel on the project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            include: /src/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
+    devtool: 'source-map',
+    watch: true
+}
