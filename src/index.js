fd6467fa08c90ee955b12333d674cad7a7de7dfd
pineladsn <bruno.pinelamarques@gmail.com>
2019-06-22 20:07:20 -0300

    import phone and path validation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import emailValidation from './email/email-validation'
 import dateTimeValidation from './dateTime/datetime-validation'
+import phoneValidation from './phone/phone-validation'
+import pathValidation from './path/path-validation'
