16d013c2e980a602a98b6f01fa32acdf87d9b701
pineladsn <bruno.pinelamarques@gmail.com>
2019-07-19 10:25:01 -0300

    Temp solution to exports all functions

--- src/index.js
+++ src/index.js
@@ -6,10 +6,44 @@ import creditCardValidation from './creditCard/creditcard-validation'
 import passwordValidation from './password/password-validation'
 
 
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+exports.emailIsValid = function(email) {
+    return emailValidation.emailIsValid(email);
 }
 
-exports.emailValidation = function(email) {
-    return emailValidation.emailIsValid(email);
+exports.dateIsValid = function(date) {
+    return dateTimeValidation.dateIsValid(date);
+}
+exports.shorttimeIsValid = function(time) {
+    return dateTimeValidation.shorttimeIsValid(time);
+}
+exports.fulltimeIsValid = function(time) {
+    return dateTimeValidation.fulltimeIsValid(time);
+}
+
+exports.phoneIsValid = function(phone) {
+    return phoneValidation.phoneIsValid(phone);
+}
+exports.phoneWithAreaCodeIsValid = function(phone) {
+    return phoneValidation.phoneWithAreaCodeIsValid(phone);
+}
+
+exports.pathIsValid = function(path) {
+    return pathValidation.pathIsValid(path);
+}
+
+exports.creditCardIsValid = function(creditcardNumber) {
+    return creditCardValidation.creditCardIsValid(creditcardNumber);
+}
+
+exports.password8To20AlphanumericIsValid = function(password) {
+    return passwordValidation.password8To20AlphanumericIsValid(password);
+}
+exports.password4To8AlphanumericIsValid = function(password) {
+    return passwordValidation.password4To8AlphanumericIsValid(password);
+}
+exports.password4To8AlphanumericWeakIsValid = function(password) {
+    return passwordValidation.password4To8AlphanumericWeakIsValid(password);
+}
+exports.password4To15AlphanumericIsValid = function(password) {
+    return passwordValidation.password4To15AlphanumericIsValid(password);
 }
