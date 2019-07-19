d2b9013c2522bd3b401f1a2a8dea0d52aeceda17
pineladsn <bruno.pinelamarques@gmail.com>
2019-07-19 11:20:15 -0300

    Fix on filename option also upgrade version

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'gg-regex.js',
+        filename: 'index.js',
         publicPath: '/dist/'
     },
     module: {
